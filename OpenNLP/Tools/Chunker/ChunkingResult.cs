b9c9459e16937188940bbe524d386546d0771226
vsjc91 <vince@dotalign.com>
2018-08-23 14:05:31 -0400

    vjs - cleanup Chunker so we can just use the interface

---
+++ OpenNLP/Tools/Chunker/ChunkingResult.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenNLP.Tools.Chunker
+{
+    public class ChunkingResult
+    {
+        public ChunkingResult(string[] tokens, string[] tags)
+        {
+            Tokens = tokens;
+            Tags = tags;
+        }
+
+        public string[] Tokens { get; }
+        public string[] Tags { get; }
+    }
+}
